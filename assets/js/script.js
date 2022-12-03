0fd7dab5a56d0df239678ba459c90a5e2c3f605e
SANJEEV KUMAR <62838848+Sanjeevkumar-kv29@users.noreply.github.com>
2022-12-03 15:44:52 +0530

    about and social updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Android Apps Development", "Python-Java Development", "", "Bigdata - hadoop,spark,data Engineering", "React development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
